4c59625e831da79030524673d7cdfbb81eba3d94
cw-skyada <skyada@codewinglet.com>
2023-10-24 18:39:26 +0530

    init commit

---
+++ src/api/demo-record/controllers/demo-record.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * demo-record controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::demo-record.demo-record');
---
+++ src/api/demo-record/routes/demo-record.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * demo-record router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::demo-record.demo-record');
